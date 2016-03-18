24ebf3515480dd45a9f8c487cff799a8c8e67bde
Alexander Samtsov <a7ex12@gmail.com>
2016-03-18 14:58:20 +1000

    UPDATE DeviceInformation activity ADD ports scan feature to detect open ports

--- app/src/main/java/com/alexandersamtsov/wifidevicesdiscovery/DeviceInformation.java
+++ app/src/main/java/com/alexandersamtsov/wifidevicesdiscovery/DeviceInformation.java
@@ -20,11 +20,17 @@
 package com.alexandersamtsov.wifidevicesdiscovery;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -32,7 +38,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,6 +52,13 @@ public class DeviceInformation extends AppCompatActivity {
     private TextView txtHostAddress;
     private TextView txtVendorName;
     private TextView txtMac;
+    private TextView txtScanPort;
+    private Button btnScanPort;
+    private PortsScanThread portsScanThread;
+
+    private ArrayList<String> openPorts;
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView lstOpenPorts;
 
 
     private static final String TAG = "DeviceInformation";
@@ -55,10 +71,9 @@ public class DeviceInformation extends AppCompatActivity {
         txtHostAddress = (TextView) findViewById(R.id.deviceinformation_hostaddress);
         txtMac = (TextView) findViewById(R.id.deviceinformation_mac);
         txtVendorName = (TextView) findViewById(R.id.deviceinformation_vendorname);
-
-
-
-
+        txtScanPort = (TextView) findViewById(R.id.deviceinformation_scanportstxt);
+        btnScanPort = (Button) findViewById(R.id.deviceinformation_scanportsbutton);
+        lstOpenPorts = (ListView) findViewById(R.id.deviceinformation_openportslist);
 
 
         hostsInfo = getIntent().getExtras().getString("hostinfo");
@@ -68,9 +83,109 @@ public class DeviceInformation extends AppCompatActivity {
         txtHostAddress.setText(hostInfoArray[0]);
         txtMac.setText(hostInfoArray[2]);
         txtVendorName.setText(hostInfoArray[1]);
+        txtScanPort.setText(getString(R.string.deviceinformation_scanportstxt));
+
+
+        openPorts = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                openPorts);
+        lstOpenPorts.setAdapter(arrayAdapter);
+
+
+        btnScanPort.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                portsScanThread = new PortsScanThread();
+                portsScanThread.execute();
+
+            }
+        });
 
 
 
+
+
+    }
+
+
+
+    private class PortsScanThread extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            txtScanPort.setText(getString(R.string.deviceinformation_scanningports));
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+            openPorts = portsScanning();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            updatedAdapter(openPorts);
+            txtScanPort.setText(getString(R.string.deviceinformation_scanningportsfinished));
+
+
+        }
+    }
+
+
+
+    private ArrayList<String> portsScanning()
+    {
+        ArrayList<String> openPorts = new ArrayList<>();
+        for(int i = 1; i < 100; i++)
+        {
+            if(isPortOpen(hostInfoArray[0], i, 20))
+            {
+                openPorts.add(String.valueOf(i));
+                Log.d(TAG, hostInfoArray[0] + " " + i + " port");
+            }
+        }
+        return openPorts;
+    }
+
+
+    private boolean isPortOpen(String ip, int port, int timeout) {
+
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(ip, port), timeout);
+            socket.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+
+    private void updatedAdapter(ArrayList<String> itemsArrayList) {
+
+        arrayAdapter.clear();
+
+        if (itemsArrayList != null){
+
+            for (String str : itemsArrayList) {
+
+                arrayAdapter.add(str);
+            }
+        }
+
+        arrayAdapter.notifyDataSetChanged();
 
     }
 
